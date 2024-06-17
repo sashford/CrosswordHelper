dfb974bafb06c8b47870a026965134cb64e798e6
sashford <spencer@theashfords.org>
2024-06-17 16:21:43 -0500

    Fixed import error

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-from WordLists import WordHelper
+from WordHelper import WordHelper
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
